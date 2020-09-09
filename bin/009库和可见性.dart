2f17d9084b23743d52c55f0206d0fbd5aabae7bf
skywalkerdll <584547953@qq.com>
2020-09-09 13:34:49 +0800

    没啥可说的

---
+++ bin/009库和可见性.dart
@@ -0,0 +1,9 @@
+import 'dart:developer';
+import 'dart:developer' as dev2; //当导入的库名称冲突的时候 使用前缀
+import 'dart:developer' show log; //只导入一部分
+import 'dart:io' deferred as http; //延迟加载
+
+void main(List<String> args) {
+  log('日志');
+  dev2.log('日志2');
+}
