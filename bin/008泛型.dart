0510ed4a9e9b8a220d9bdccebb45e2dbc1f7c413
skywalkerdll <584547953@qq.com>
2020-09-09 11:03:49 +0800

    第三次提交

--- bin/008泛型.dart
+++ bin/008泛型.dart
@@ -1,3 +1,13 @@
 void main(List<String> args) {
-  print('111');
+  //泛型
+  var names = <String>['Seth', 'Kathy', 'Lars'];
+  var uniqueNames = <String>{'Seth', 'Kathy', 'Lars'};
+  var pages = <String, String>{
+    'index.html': 'Homepage',
+    'robots.txt': 'Hints for web robots',
+    'humans.txt': 'We are people, not machines'
+  };
+  print(names);
+  print(uniqueNames);
+  print(pages);
 }
