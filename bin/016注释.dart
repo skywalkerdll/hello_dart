c604666af3fc178fdcfd5027d81ddeed6e7b0436
skywalkerdll <584547953@qq.com>
2020-09-12 15:33:06 +0800

    ddd

--- bin/016注释.dart
+++ bin/016注释.dart
@@ -1,4 +1,14 @@
 void main(List<String> args) {
-  //注释
+  //这是一个单行注释
   print('注释');
+  /*
+    这是一个多行注释
+  */
+  print('多行注释');
+
+  /// A domesticated South American camelid (Lama glama).
+  ///
+  /// 自从西班牙时代以来，
+  /// 安第斯文化就将骆驼当做肉食类和运输类动物。
+  print('文档注释');
 }
