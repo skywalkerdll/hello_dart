cd62c2453e2419d2414da5f1682f926d2f4a52c4
skywalkerdll <584547953@qq.com>
2020-09-12 16:27:44 +0800

    update alllllllllllllllllllllllllllllllll

--- bin/010异步支持.dart
+++ bin/010异步支持.dart
@@ -1,28 +1,39 @@
+import 'dart:io';
+
 String method1() {
   return '1';
 }
 
 String method2() {
-  return '2';
+  //暂停5秒
+  sleep(Duration(seconds: 5));
+  return 'get netdata';
 }
 
-String method3() {
-  return '3';
+Future<String> method3() {
+  return Future<String>(() {
+    sleep(Duration(seconds: 5));
+    throw Exception('草率了');
+    //return 'get netdata';
+  });
 }
 
-Future<String> method5() async {
-  return '5';
-}
-
-void main(List<String> args) async {
-  //异步
+void main(List<String> args) {
+  print('main funciton begin');
+  //同步调用
   var f1 = method1();
-  print(f1);
-  method2();
-  var f2 = method2();
-  print(f2);
-  var f5 = await method5();
-  print(f5);
+  print('调用f1:' + f1);
+  //同步调用f2
+  // var f2 = method2();
+  // print('调用f2 阻塞5秒:' + f2);
+  //异步调用f3
   var f3 = method3();
+  //使用lfeture后如何获取执行结果
+  f3.then((value) {
+    print(value);
+  }).catchError((Err) {
+    print(Err);
+  });
   print(f3);
+  print('main function end');
 }
